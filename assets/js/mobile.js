95b48835e1e236c0268fcf1a09cec83b99ea0a9b
Jovanie24 <paraderojovanie_act@plmun.edu.ph>
2022-12-27 18:58:54 +0800

    Updated Index & css

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -10,3 +10,29 @@ menuOpen.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   overlay.classList.remove("overlay--active");
 });
+
+// Form validation code will come here.
+      function validate() {
+      
+         if( document.myForm.name.value == "" ) {
+            document.getElementById('error').innerHTML = "Name is required.";
+            return false;
+         }
+         else if( document.myForm.email.value == "" ) {
+            document.getElementById('error').innerHTML = "Email is required.";
+            return false;
+         }
+         else if( document.myForm.code.value == "" ) {
+            document.getElementById('error').innerHTML = "Code is required.";
+            return false;
+         }
+         else if( document.myForm.phone.value == "" ) {
+            document.getElementById('error').innerHTML = "Phone is required.";
+            return false;
+         }
+            document.getElementById('success').innerHTML = "Thanks for contacting us! We will get in touch with you shortly.";
+            setTimeout(function(){
+   window.location.reload();
+}, 3000);
+            return false;
+}
